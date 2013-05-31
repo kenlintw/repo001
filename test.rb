887d07ef6722a0db84e1700ec23d0ce7f62f9b26
kenlintw <kenl5487@gmail.com>
2013-05-31 11:36:42 +0800

    hotfix

--- test.rb
+++ test.rb
@@ -2,4 +2,6 @@ this is test.rb
 add one line
 add one line also make a branch at this point
 add one line after create branch branch_0530
+# hotfix for urgent call from customer
+fix urgent bugs found by customer
 
